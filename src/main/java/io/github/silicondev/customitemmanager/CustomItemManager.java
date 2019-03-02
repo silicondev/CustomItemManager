90ea594eb84cfcb9033529ccf0e0d4d22f5d4656
silicondev <47757443+silicondev@users.noreply.github.com>
2019-03-02 02:18:48 +0000

    Beta 0.2.0 Testing Complete
    No change, just testing complete

--- src/main/java/io/github/silicondev/customitemmanager/CustomItemManager.java
+++ src/main/java/io/github/silicondev/customitemmanager/CustomItemManager.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class CustomItemManager extends JavaPlugin {
 	public static String pluginName = "CustomItemManager";
 	public boolean debugMode = false;
-	public static String version = "Beta 0.1.8";
+	public static String version = "Beta 0.2.0";
 	private FileConfiguration itemConfig;
 	public static YamlConfiguration langConfig;
 	private File itemFile = new File(getDataFolder(), "items.yml");
